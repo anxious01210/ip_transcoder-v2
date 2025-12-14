525662262e2058bf09b3a02628f999a1a1210c05
anxious01210 <pinktheory21@gmail.com>
2025-12-14 10:40:06 +0300

    Switch database from SQLite to PostgreSQL

--- iptranscoder/settings.py
+++ iptranscoder/settings.py
@@ -70,13 +70,22 @@ WSGI_APPLICATION = 'iptranscoder.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "ip_transcoder_v2_db",
+        "USER": "ip_transcoder_v2_user",
+        "PASSWORD": "Diagonal1!DB",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -111,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
