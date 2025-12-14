c1ba82e2a5c7035089d301bf8a7eeb4f573d7a86
anxious01210 <pinktheory21@gmail.com>
2025-12-14 12:33:25 +0300

    Update ALLOWED_HOSTS for development environment

--- iptranscoder/settings.py
+++ iptranscoder/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-64%vr^h22_vm37&100e*iaf%z7yp24)r81k6r!h!tht0bl@4q3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "*"]
 
 # Application definition
 
